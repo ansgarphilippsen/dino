e943036b16b3672faa12ff65112fd02b7aac2c9d
Ansgar Philippsen <ansgar.philippsen@gmail.com>
2001-02-01 09:50:10 +0000

    modified sgi video detection

--- src/sgi_video.c
+++ src/sgi_video.c
@@ -12,8 +12,12 @@ char *sgi_video_dir[]={
   ""
 };
 
+const int sgi_video_dir_count=6;
+
 char sgi_video_static_buf[2048];
 char *sgi_video_static_list[256];
+
+#define SGI_NEW_DETECTION
 
 int sgi_video_get_list(int * lc, char ***l)
 {
@@ -21,11 +25,28 @@ int sgi_video_get_list(int * lc, char ***l)
   DIR *d;
   struct direct *dp;
   char *lp;
+  FILE *f;
+  char buf[256];
 
   /* detect graphics board */
   
+#ifdef SGI_NEW_DETECTION
+  f=popen("find /usr/gfx/ucode -name '*x*_*s.*'","r");
+  i=0;
+  lp=sgi_video_static_buf;
+  memset(lp,0,2048);
+  sgi_video_static_list[0]=lp;
+  while(!feof(f)) {
+    memset(buf, 0, 256);
+    fgets(buf, 256, f);
+    strncpy(lp,strlen(buf),256);
+    lp[strlen(buf)-1]='\0';
+    sgi_video_static[++i]=lp+strlen(buf);
+  }
+  pclose(f);
+#else
   d=NULL;
-  for(i=0;sgi_video_dir[i][0]!='\0';i++) {
+  for(i=0;i<sgi_video_dir_count;i++) {
     d=opendir(sgi_video_dir[i]);
     if(d!=NULL)
       break;
@@ -50,6 +71,7 @@ int sgi_video_get_list(int * lc, char ***l)
 
   (*lc)=i;
   (*l)=sgi_video_static_list;
-
+#endif
   return 0;
 }
+
