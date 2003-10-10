0b3686ca8e49d92bd9538e7bd5f3a60ed6e7e5e6
Ansgar Philippsen <ansgar.philippsen@gmail.com>
2003-10-10 12:57:27 +0000

    mouse handling for wx implemented

--- src/wx/gui_wx.cpp
+++ src/wx/gui_wx.cpp
@@ -41,6 +41,45 @@ public:
   }
 
   virtual void OnMouse(wxMouseEvent& e) {
+    if(e.ButtonUp() || e.ButtonDown() || e.Dragging() ) {
+      cmiToken t;
+      int val[5];
+      int i;
+      int dx,dy;
+      
+      t.target=CMI_TARGET_COM;
+      t.command=CMI_INPUT;
+      t.value=val;
+      
+      val[0]=CMI_INPUT_MOUSE;
+
+      val[2]=0;
+
+      if(e.ButtonDown()) {
+	val[1]=CMI_BUTTON_PRESS;
+	if(e.LeftDown())   val[2] += CMI_BUTTON1_MASK;
+	if(e.RightDown())  val[2] += CMI_BUTTON3_MASK;
+	if(e.MiddleDown()) val[2] += CMI_BUTTON2_MASK;
+      } else if(e.ButtonUp()) {
+	val[1]=CMI_BUTTON_RELEASE;
+	if(e.LeftUp())   val[2] += CMI_BUTTON1_MASK;
+	if(e.RightUp())  val[2] += CMI_BUTTON3_MASK;
+	if(e.MiddleUp()) val[2] += CMI_BUTTON2_MASK;
+      } else {
+	val[1]=CMI_MOTION;
+	if(e.LeftIsDown())   val[2] += CMI_BUTTON1_MASK;
+	if(e.RightIsDown())  val[2] += CMI_BUTTON3_MASK;
+	if(e.MiddleIsDown()) val[2] += CMI_BUTTON2_MASK;
+      }
+
+      if(e.ShiftDown())    val[2] += CMI_SHIFT_MASK;
+      if(e.ControlDown())  val[2] += CMI_CNTRL_MASK;
+      
+      val[3]=e.GetX();
+      val[4]=e.GetY();
+      
+      cmiSubmit(&t);
+    }    
   }
 
   virtual void OnChar(int kc) {
@@ -49,6 +88,8 @@ public:
   virtual void OnTimer() {
     cmiTimer();
   }
+
+private:
 };
 
 class DinoShell: public Shell {
@@ -145,8 +186,9 @@ void guiCMICallback(const cmiToken *t)
 {
   if(t->target==CMI_TARGET_GUI) {
     switch(t->command) {
-    case CMI_REFRESH: dino_glview->Refresh(false); break;
+    case CMI_REFRESH: dino_glview->RefreshGL(); break;
     case CMI_MESSAGE: guiMessage((char *)t->value); break;
+    case CMI_CHECKR: dino_glview->RefreshGL(); break;
     }
   }
 }
